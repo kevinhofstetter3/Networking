4264e4c89525376362bb61caf3c5e047dbd7ac45
Kevin Hofstetter <kevhoff3@gmail.com>
2021-12-04 19:54:21 -0500

    INIT AI

---
+++ AI/Predictions/stock_price.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential, load_model
+from keras.layers import LSTM, Dense, Dropout
+
+if __name__ == "__main__":
